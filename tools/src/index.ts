230599addff6c1525da87131f1483ae40fd4afcf
bibek190 <bibek9818917036@gmail.com>
2026-03-14 00:13:42 +1100

    tool calling

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,81 +1,96 @@
 import { OpenAI } from "openai";
 import dotenv from "dotenv";
-
+import { tavily } from "@tavily/core";
 dotenv.config();
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY is missing in .env");
+}
+
+if (!process.env.TAVILY_API_KEY) {
+  throw new Error("TAVILY_API_KEY is missing in .env");
+}
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const tavilyClient = tavily({
+  apiKey: process.env.TAVILY_API_KEY,
+});
 
-async function getNewYorkTime() {
-  return new Date().toLocaleString("en-US", {
-    timeZone: "America/New_York",
-  });
+async function getUpdate({ query }) {
+  const response = await tavilyClient.search(query);
+  const data = response.results?.map((result) => result.content).join("\n\n");
+  return data;
 }
 
-function calculator(a, b) {
-  return a + b;
-}
+const messages = [
+  {
+    role: "system",
+    content:
+      "You are a helpful assistant that provides concise answers to questions.",
+  },
+  {
+    role: "user",
+    content: "When was iPhone 17 launched?",
+  },
+];
+
+const tools = [
+  {
+    type: "function",
+    function: {
+      name: "getUpdate",
+      description:
+        "Get the latest information from the websearch and realtime database",
+      parameters: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            description: "Search query for retrieving latest information",
+          },
+        },
+        required: ["query"],
+      },
+    },
+  },
+];
 
 async function run() {
   const response = await client.chat.completions.create({
-    model: "gpt-4.1-mini",
-    messages: [
-      {
-        role: "user",
-        content: "What is 33+33 and what time is it in New York?",
-      },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "getNewYorkTime",
-          description: "Get the current time of new York",
-          parameters: {
-            type: "object",
-            properties: {},
-          },
-        },
-      },
-      {
-        type: "function",
-        function: {
-          name: "calculator",
-          description: "A simple calculator that can add two numbers",
-          parameters: {
-            type: "object",
-            properties: {
-              a: {
-                type: "number",
-                description: "The first number",
-              },
-              b: {
-                type: "number",
-                description: "The second number",
-              },
-            },
-            required: ["a", "b"],
-          },
-        },
-      },
-    ],
+    model: "gpt-4o",
+    messages,
+    tools: tools,
+    tool_choice: "auto",
   });
-  const message = response.choices[0].message;
-  if (message.tool_calls) {
-    const toolCall = message.tool_calls[0];
-    const toolName = toolCall.function.name;
-
-    if (toolName === "calculator") {
-      const args = JSON.parse(toolCall.function.arguments);
-      const result = calculator(args.a, args.b);
-      console.log("Tool result:", result);
-    }
-    if (toolName === "getNewYorkTime") {
-      const result = await getNewYorkTime();
-
-      console.log("Tool result:", result);
+  messages.push(response.choices[0].message);
+  const tool_calls = response.choices[0].message.tool_calls;
+  if (!tool_calls) {
+    console.log("Assistant", response.choices[0].message.content);
+    return;
+  }
+  for (const tool of tool_calls) {
+    const toolName = tool.function.name;
+    const args = JSON.parse(tool.function.arguments);
+    // console.log("toolname:", toolName);
+    if (toolName === "getUpdate") {
+      const tool_Response = await getUpdate(args);
+      messages.push({
+        role: "tool",
+        name: toolName,
+        tool_call_id: tool.id,
+        content: tool_Response,
+      });
+      // console.log("tool response:", tool_Response);
     }
   }
+  const finalResponse = await client.chat.completions.create({
+    model: "gpt-4o",
+    messages,
+    tools: tools,
+    tool_choice: "auto",
+  });
+  console.log("Assistant : ", finalResponse.choices[0].message.content);
 }
 run();
